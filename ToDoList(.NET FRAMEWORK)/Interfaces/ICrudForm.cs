78bec07c37cf1a793abe051efbe0b7299fcb5476
Dhane <ninadhanerobbie.robinos29@gmail.com>
2025-06-25 20:24:59 +0800

    Add ICrudForm interface for standardized CRUD methods across forms

---
+++ ToDoList(.NET FRAMEWORK)/Interfaces/ICrudForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList_.NET_FRAMEWORK_.Interfaces
+{
+    public interface ICrudForm
+    {
+        void AddEntry();
+        void UpdateEntry();
+        void DeleteEntry();
+    }
+}
